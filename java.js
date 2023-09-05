aa3569885f547a96d7d239b2c5d57e6b7570309c
luisolascano <137640862+luisolascano@users.noreply.github.com>
2023-09-04 20:34:49 -0400

    Added a bunch of stuff

--- java.js
+++ java.js
@@ -1,53 +1,109 @@
-// combined.js
+document.addEventListener('DOMContentLoaded', function() {
 
-// Get a reference to the button and the projects section
-const toggleButton = document.getElementById('toggleProjects');
-const projectsSection = document.getElementById('projects');
+    // Get a reference to the button and the projects section
+    const toggleButton = document.getElementById('toggleProjects');
+    const projectsSection = document.getElementById('projects');
 
-// Set an initial flag to track the visibility state
-let projectsVisible = false;
+    // Check if the elements exist before proceeding
+    if (toggleButton && projectsSection) {
 
-// Function to toggle the visibility of the projects section
-function toggleProjects() {
-    projectsVisible = !projectsVisible;
-    projectsSection.style.display = projectsVisible ? 'block' : 'none';
-    toggleButton.textContent = projectsVisible ? 'Hide Projects' : 'Show Projects';
-}
+        // Set an initial flag to track the visibility state
+        let projectsVisible = false;
 
-// Add a click event listener to the button
-toggleButton.addEventListener('click', toggleProjects);
+        // Function to toggle the visibility of the projects section
+        function toggleProjects() {
+            projectsVisible = !projectsVisible;
+            projectsSection.style.display = projectsVisible ? 'block' : 'none';
+            toggleButton.textContent = projectsVisible ? 'Hide Projects' : 'Show Projects';
+        }
 
-// Initially hide the projects section
-projectsSection.style.display = 'none';
+        // Add a click event listener to the button
+        toggleButton.addEventListener('click', toggleProjects);
 
-// Get a reference to the timer div
-const timerDiv = document.getElementById('timer');
+        // Initially hide the projects section
+        projectsSection.style.display = 'none';
+    }
 
-// Function to update the timer
-function updateTimer() {
-    // Check if the 'visitTime' cookie exists
-    const visitTime = parseInt(getCookie('visitTime')) || 0;
+    // Get a reference to the timer div
+    const timerDiv = document.getElementById('timer');
 
-    // Calculate the time spent on the page in seconds
-    const now = new Date().getTime();
-    const timeOnPage = Math.floor((now - visitTime) / 1000);
+    // Initialize the timer value
+    let timeOnPage = 0;
+    let save = false; // Initialize 'save' as false
 
-    // Display the time on the page
-    timerDiv.textContent = `And if you were wondering you have been on this page for ${timeOnPage} seconds`;
+    // Function to update the timer
+    function updateTimer() {
+        // Update the 'timeOnPage' variable
+        timeOnPage++;
 
-    // Store the current time as the 'visitTime' cookie
-    document.cookie = `visitTime=${now}`;
-}
+        // Display the time on the page
+        timerDiv.textContent = `You have been on this page for ${timeOnPage} seconds`;
 
-// Function to get the value of a cookie
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
+        // Save the updated timer value as a cookie if 'save' is true
+        if (save) {
+            document.cookie = `timeOnPage=${timeOnPage}; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/`;
+        }
+    }
 
-// Update the timer every second
-setInterval(updateTimer, 1000);
+    // Update the timer every second
+    const timerInterval = setInterval(updateTimer, 1000);
 
-// Call the function to initially set the timer
-updateTimer();
+
+    // Cookie Consent Banner
+    const cookieConsentBanner = document.getElementById('cookieConsentBanner');
+    const acceptCookiesButton = document.getElementById('acceptCookies');
+    const rejectCookiesButton = document.getElementById('rejectCookies');
+
+    // Check if the user has previously accepted or rejected cookies
+    const hasAcceptedCookies = localStorage.getItem('hasAcceptedCookies');
+
+    if (!hasAcceptedCookies) {
+        // Show the cookie consent banner if the user hasn't made a choice
+        cookieConsentBanner.style.display = 'block';
+
+        // Handle click event for "Accept" button
+        acceptCookiesButton.addEventListener('click', function() {
+            // Set 'save' to true before starting the timer
+            save = true;
+
+            // Set a flag in local storage to remember the user's choice
+            localStorage.setItem('hasAcceptedCookies', 'true');
+
+            // Hide the banner
+            cookieConsentBanner.style.display = 'none';
+
+        });
+
+        // Handle click event for "Reject" button
+        rejectCookiesButton.addEventListener('click', function() {
+            // Set a flag in local storage to remember the user's choice
+            localStorage.setItem('hasAcceptedCookies', 'false');
+
+            // Hide the banner
+            cookieConsentBanner.style.display = 'none';
+        });
+    } else {
+        // If the user has previously accepted cookies, set 'save' to true
+        save = true;
+    }
+    // Function to get the value of a cookie by name
+    function getCookie(cookieName) {
+        const cookies = document.cookie.split(';');
+        for (const cookie of cookies) {
+            const [name, value] = cookie.trim().split('=');
+            if (name === cookieName) {
+                return value;
+            }
+        }
+        return null;
+    }
+    const cookieValue = parseInt(getCookie('timeOnPage'));
+    if (!isNaN(cookieValue)) {
+        timeOnPage = timeOnPage + cookieValue;
+
+
+    }
+
+    // Call the function to initially set the timer
+    updateTimer();
+});
